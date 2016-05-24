d2c32b563571aee0be0683cc97c2f3287f00647f
Gen Liu <gerald_721@hotmail.com>
2016-05-24 22:41:22 +0800

    fix for resource

--- mongoose-entity.js
+++ mongoose-entity.js
@@ -4,11 +4,11 @@ module.exports = function(opts) {
   var mongoose;
 
   seneca.add('init:mongoose-entity', init);
-  seneca.add('role:mongoose-entity, cmd:find-all', findAll);
+  seneca.add('role:mongoose-entity, cmd:findAll', findAll);
   seneca.add('role:mongoose-entity, cmd:create', create);
-  seneca.add('role:mongoose-entity, cmd:find-by-id', findById);
-  seneca.add('role:mongoose-entity, cmd:update-by-id', updateById);
-  seneca.add('role:mongoose-entity, cmd:destroy-by-id', destroyById);
+  seneca.add('role:mongoose-entity, cmd:findById', findById);
+  seneca.add('role:mongoose-entity, cmd:updateById', updateById);
+  seneca.add('role:mongoose-entity, cmd:destroyById', destroyById);
 
   return;
 
@@ -21,11 +21,16 @@ module.exports = function(opts) {
     var model = args.model;
     var Model = mongoose.model(model);
 
-    console.log('model', model);
+    var limit = args.limit;
+    var skip = args.skip;
+    var sort = args.sort;
+    var filters = args.filters;
+    // console.log('==========findall==========');
+    // console.log(args);
 
     Promise.all([
-      Model.count(),
-      Model.find()
+      Model.count(filters),
+      Model.find(filters).limit(limit).skip(skip).sort(sort)
     ]).then(function(results) {
       done(null, {
         count: results[0],
@@ -41,7 +46,7 @@ module.exports = function(opts) {
 
     var entity = new Model(data);
     entity.save().then(function() {
-      done(null, entity);
+      done(null, entity.toObject());
     }).catch(done);
   }
 
@@ -51,7 +56,8 @@ module.exports = function(opts) {
     var Model = mongoose.model(model);
 
     Model.findById(id).then(function(entity) {
-      done(null, entity)
+      if (!entity) throw new Error('not fount');
+      done(null, entity.toObject())
     }).catch(done);
   }
 
@@ -62,7 +68,8 @@ module.exports = function(opts) {
     var Model = mongoose.model(model);
 
     Model.findByIdAndUpdate(id, data).then(function(entity) {
-      done(null, entity)
+      if (!entity) throw new Error('not fount');
+      done(null, entity.toObject())
     }).catch(done);
   }
 
@@ -72,7 +79,8 @@ module.exports = function(opts) {
     var Model = mongoose.model(model);
 
     Model.findByIdAndRemove(id).then(function(entity) {
-      done(null, entity)
+      if (!entity) throw new Error('not fount');
+      done(null, entity.toObject())
     }).catch(done);
   }
 
